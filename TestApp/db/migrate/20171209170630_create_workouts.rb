a44c66c204fc1aea816a3fd0ef27dd8fd29763f7
Daniel Levenson <daniellevenson4@gmail.com>
2017-12-09 12:16:35 -0500

    ran db:migrate, populated seeds file with sample data

--- TestApp/db/migrate/20171209170630_create_workouts.rb
+++ TestApp/db/migrate/20171209170630_create_workouts.rb
@@ -1,7 +1,12 @@
 class CreateWorkouts < ActiveRecord::Migration[5.1]
   def change
     create_table :workouts do |t|
-
+    	t.string :name
+    	t.string :muscle_group
+    	t.integer :sets
+    	t.integer :reps
+    	t.integer :weight
+    	t.string :date
       t.timestamps
     end
   end
