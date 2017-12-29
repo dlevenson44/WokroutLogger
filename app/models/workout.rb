b7947395ddb800bcfe876d1159b9c20e0b408e83
Daniel Levenson <daniellevenson4@gmail.com>
2017-12-29 00:51:12 -0500

    fixed field requirements

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,3 @@
 class Workout < ApplicationRecord
-	validates :name, :muscle_group, :sets, :reps, :date, :presence => true
+	# validates :name, :muscle_group, :sets, :reps, :date, :presence => true
 end
